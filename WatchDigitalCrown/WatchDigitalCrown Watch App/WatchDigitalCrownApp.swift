766873183d60a9c76d09006e4fafa79890eae250
Sarah Clark <sarahireneclark@gmail.com>
2023-10-29 20:10:13 -0500

    Initial commit of project

---
+++ WatchDigitalCrown/WatchDigitalCrown Watch App/WatchDigitalCrownApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchDigitalCrownApp.swift
+//  WatchDigitalCrown Watch App
+//
+//  Created by Sarah Clark on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchDigitalCrown_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
