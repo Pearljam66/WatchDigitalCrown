00a88c4cea8f5566c0cd8be18065292c7071493b
Sarah Clark <sarahireneclark@gmail.com>
2023-10-30 18:46:25 -0500

    Added code for the digital crown settings

--- WatchDigitalCrown/WatchDigitalCrown Watch App/Views/ContentView.swift
+++ WatchDigitalCrown/WatchDigitalCrown Watch App/Views/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("\(number, specifier: "%.1f")")
+            .onChange(of: number) { newValue, _ in
+                print(newValue)
+            }
+            .focusable()
+            .digitalCrownRotation (
+                $number,
+                from: 0.0,
+                through: 12.0,
+                by: 0.1,
+                sensitivity: .low,
+                isContinuous: true,
+            isHapticFeedbackEnabled: true)
     }
 }
 
